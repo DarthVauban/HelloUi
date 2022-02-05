a88bdc07b3fee71c7478696099fa86b2de9ef216
DarthVauban <Dante2770@gmail.com>
2022-02-05 22:46:57 +0200

    !

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 $('.partners-slider').slick({
-    infinite: true,
+    infinite: false,
     arrows: false,
     slidesToShow: 6,
     slidesToScroll: 3,
     dots: true,
     appendDots: $('.partners-slider-dots'),
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 });
